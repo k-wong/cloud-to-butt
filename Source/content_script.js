a95eb4b121bd37652e78d91128d3e2aff7905c26
K <kevin@Ks-MacBook-Air.local>
2022-01-21 12:04:02 -0500

    Initial commit

--- Source/content_script.js
+++ Source/content_script.js
@@ -39,10 +39,8 @@ function handleText(textNode)
 {
 	var v = textNode.nodeValue;
 
-	v = v.replace(/\bThe Cloud\b/g, "My Butt");
-	v = v.replace(/\bThe cloud\b/g, "My butt");
-	v = v.replace(/\bthe Cloud\b/g, "my Butt");
-	v = v.replace(/\bthe cloud\b/g, "my butt");
+	v = v.replace(/\bNFT\b/g, "silly jpg");
+	v = v.replace(/\bnft\b/g, "silly jpg");
 	
 	textNode.nodeValue = v;
 }
